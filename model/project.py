095b565438a135bf6616f794b51bc378c72de22e
Quentin Tawry <qt@abakusitsolutions.eu>
2016-02-05 15:32:21 +0100

    NOT STABLE
    Filtering started have to include OR in the filter

--- model/project.py
+++ model/project.py
@@ -37,19 +37,61 @@ class project_project_report_methods(models.Model):
         return self.__get_project_report().issues
     def issues_type(self):
         return self.__get_project_report().issues_type
+
     def display_tasks(self):
         return self.__get_project_report().tasks
     def tasks_type(self):
         return self.__get_project_report().tasks_type
+
     def get_comments(self):
         return self.__get_project_report().comments
+
     def display_project_times(self):
         return self.__get_project_report().project_times
     def display_project_info(self):
         return self.__get_project_report().project_info
+
     def display_charts(self):
         return self.__get_project_report().show_chart
+
     def startdate(self):
         return self.__get_project_report().start_date
     def enddate(self):
         return self.__get_project_report().end_date
+
+
+    def date_application_creation(self):
+        return self.__get_project_report().date_application_creation
+    def date_application_modified(self):
+        return self.__get_project_report().date_application_modified
+    def date_application_closed(self):
+        return self.__get_project_report().date_application_closed
+
+    def get_filter(self):
+        filter = []
+
+        startdate = self.startdate() if self.startdate() else datetime.now().strftime('%y-%m%d')
+        enddate = self.enddate() if self.enddate() else datetime.now().strftime('%y-%m%d')
+        created = self.date_application_creation()
+        modified = self.date_application_modified()
+        closed = self.date_application_closed()
+
+        filter.append(('project_id','=',self.id))
+
+        if created:
+            filter.append(('create_date', '>=',startdate))
+            filter.append(('create_date','<=',enddate))
+        if modified:
+            filter.append(('date_action_last','>=', startdate))
+            filter.append(('date_action_last','<=', enddate))
+        if closed:
+            filter.append(('date_closed','>=', startdate))
+            filter.append(('date_closed','>=', enddate))
+        _logger.debug(filter)
+        return filter
+
+    def get_issues_for_report(self):
+        cr = self.env.cr
+        uid = self.env.user.id
+        project_issues = self.pool.get('project.issue').search(cr, uid, self.get_filter())
+        return project_issues
