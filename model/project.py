f324595227b39965891290a2dc752894f92661b1
Quentin Tawry <qt@abakusitsolutions.eu>
2016-02-11 16:51:46 +0100

    Filter finished
    still need the order by

--- model/project.py
+++ model/project.py
@@ -37,11 +37,15 @@ class project_project_report_methods(models.Model):
         return self.__get_project_report().issues
     def issues_type(self):
         return self.__get_project_report().issues_type
+    def issues_stage(self):
+        return self.__get_project_report().issues_stage
 
     def display_tasks(self):
         return self.__get_project_report().tasks
     def tasks_type(self):
         return self.__get_project_report().tasks_type
+    def tasks_stage(self):
+        return self.__get_project_report().tasks_stage
 
     def get_comments(self):
         return self.__get_project_report().comments
@@ -67,8 +71,8 @@ class project_project_report_methods(models.Model):
     def date_application_closed(self):
         return self.__get_project_report().date_application_closed
 
-    def get_filter(self):
-        filter = []
+    def get_filter_issues(self):
+        filter = ['&']
 
         startdate = self.startdate() if self.startdate() else datetime.now().strftime('%y-%m%d')
         enddate = self.enddate() if self.enddate() else datetime.now().strftime('%y-%m%d')
@@ -78,20 +82,91 @@ class project_project_report_methods(models.Model):
 
         filter.append(('project_id','=',self.id))
 
+        count = 0 + created + modified + closed
+        if (count>=2):
+            filter.append('|') # if >= 2
+
         if created:
+            filter.append('&')
             filter.append(('create_date', '>=',startdate))
             filter.append(('create_date','<=',enddate))
+
+        if count == 3:
+            filter.append('|') # if = 3
+
         if modified:
-            filter.append(('date_action_last','>=', startdate))
-            filter.append(('date_action_last','<=', enddate))
+            filter.append('&')
+            filter.append(('write_date', '>=',startdate))
+            filter.append(('write_date','<=',enddate))
         if closed:
-            filter.append(('date_closed','>=', startdate))
-            filter.append(('date_closed','>=', enddate))
+            filter.append('&')
+            filter.append(('date_closed', '>=',startdate))
+            filter.append(('date_closed','<=',enddate))
+
+        if self.issues_stage() == 'open':
+            filter.append(('stage_id.closed', '=', False))
+        elif self.issues_stage() == 'closed':
+            filter.append(('stage_id.closed', '=', True))
         _logger.debug(filter)
+
         return filter
 
+    def get_filter_tasks(self):
+        filter = ['&']
+
+        startdate = self.startdate() if self.startdate() else datetime.now().strftime('%y-%m%d')
+        enddate = self.enddate() if self.enddate() else datetime.now().strftime('%y-%m%d')
+        created = self.date_application_creation()
+        modified = self.date_application_modified()
+        closed = self.date_application_closed()
+
+        filter.append(('project_id','=',self.id))
+
+        count = 0 + created + modified + closed
+        if (count>=2):
+            filter.append('|') # if >= 2
+
+        if created:
+            filter.append('&')
+            filter.append(('create_date', '>=',startdate))
+            filter.append(('create_date','<=',enddate))
+
+        if count == 3:
+            filter.append('|') # if = 3
+
+        if modified:
+            filter.append('&')
+            filter.append(('write_date', '>=',startdate))
+            filter.append(('write_date','<=',enddate))
+        if closed:
+            filter.append('&')
+            filter.append(('date_end', '>=',startdate))
+            filter.append(('date_end','<=',enddate))
+        if self.tasks_stage() == 'open':
+            filter.append(('stage_id.closed', '=', False))
+        elif self.tasks_stage() == 'closed':
+            filter.append(('stage_id.closed', '=', True))
+        _logger.debug(filter)
+
+        return filter
+        #return ['&', ('create_date', '>=', '2016-02-11'), ('create_date', '<=', '2016-02-11')]
+
+    @api.multi
     def get_issues_for_report(self):
         cr = self.env.cr
         uid = self.env.user.id
-        project_issues = self.pool.get('project.issue').search(cr, uid, self.get_filter())
+        project_issues = self.pool.get('project.issue').search(cr, uid, self.get_filter_issues())
+        project_issues = self.pool.get('project.issue').browse(cr,uid,project_issues)
+        _logger.debug('Issues : ')
+        _logger.debug(project_issues)
         return project_issues
+
+    @api.multi
+    def get_tasks_for_report(self):
+        cr = self.env.cr
+        uid = self.env.user.id
+        project_tasks = self.pool.get('project.task').search(cr, uid, self.get_filter_tasks())
+        project_tasks = self.pool.get('project.task').browse(cr,uid,project_tasks)
+        _logger.debug('Tasks : ')
+        _logger.debug(project_tasks)
+        return project_tasks
