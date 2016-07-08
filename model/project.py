6cf9f3c88e5721244d72a604e7e2b05159fe3e34
Valentin Thirion <valentinthirion@gmail.com>
2016-07-08 12:18:42 +0200

    Improved button access to wizard

--- model/project.py
+++ model/project.py
@@ -21,19 +21,13 @@ class project_project_report_methods(models.Model):
 
     # ----- ACTION METHODS
     def action_open_print_report_wizard(self, cr, uid, ids, context=None):
-        dummy, view_id = self.pool.get('ir.model.data').get_object_reference(cr, uid, 'project_report', 'view_project_report_wizard_print')
-        #self.pool.get('project.report.wizard').reset_stats(cr, uid)
         return {
-            'name':_("Print Service Report"),
-            'view_mode': 'form',
-            'view_id': view_id,
-            'view_type': 'form',
-            'res_model': 'project.report.wizard',
-            'res_id': 1,
             'type': 'ir.actions.act_window',
-            'target': 'new',
+            'name': 'Print Service Report',
+            'res_model': 'project.report.wizard',
             'domain': '',
-            'context': {'project_ids': ids,}
+            'view_mode': 'form',
+            'target': 'new',
         }
 
     # ----- MODEL METHODS
